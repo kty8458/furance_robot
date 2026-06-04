42b57cc9519f11390c5e87f22ee7fc0821f6e69b
ketianyang <563646@baosight.com>
2026-06-04 10:38:39 +0800

    chore: hide commands and logs pages from sidebar

--- robot_control/frontend/src/App.vue
+++ robot_control/frontend/src/App.vue
@@ -4,11 +4,9 @@
       <div class="tech-title">机器人控制</div>
       <el-menu :default-active="$route.path" router background-color="transparent" text-color="#6b7b8d" active-text-color="#00d4ff">
         <el-menu-item index="/"><el-icon><Monitor /></el-icon><span>状态监控</span></el-menu-item>
-        <el-menu-item index="/commands"><el-icon><Operation /></el-icon><span>指令面板</span></el-menu-item>
         <el-menu-item index="/arm"><el-icon><SetUp /></el-icon><span>上身运控</span></el-menu-item>
         <el-menu-item index="/navigation"><el-icon><MapLocation /></el-icon><span>导航</span></el-menu-item>
         <el-menu-item index="/ros2"><el-icon><Cpu /></el-icon><span>ROS2节点</span></el-menu-item>
-        <el-menu-item index="/logs"><el-icon><Document /></el-icon><span>运行日志</span></el-menu-item>
         <el-menu-item index="/workflow"><el-icon><List /></el-icon><span>工作流</span></el-menu-item>
         <el-menu-item index="/camera"><el-icon><View /></el-icon><span>相机</span></el-menu-item>
       </el-menu>
@@ -19,6 +17,6 @@
 </template>
 
 <script setup>
-import { Monitor, Operation, SetUp, MapLocation, Cpu, Document, List, View } from '@element-plus/icons-vue'
+import { Monitor, SetUp, MapLocation, Cpu, List, View } from '@element-plus/icons-vue'
 import ArmEnableFab from './components/ArmEnableFab.vue'
 </script>
